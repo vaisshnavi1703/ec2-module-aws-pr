f5035511238e7a6f05866515d5e0b2fea01987a6
vaisshnavi1703 <potlapallivaisshnavi@gmail.com>
2025-05-23 11:27:29 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "ec2_sg" {
+resource "aws_security_group" "ec2_sg" { # change
   name        = "ec2_sg"
   description = "Allow SSH and HTTP"
   vpc_id      = var.vpc_id
